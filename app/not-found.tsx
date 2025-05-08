b05d9d353130edd2f80ee6b6b7089a7435b7ee89
Caio Santana <brnoliveirasantana@gmail.com>
2025-05-07 21:48:09 -0300

    Add custom button component and enhance NotFound page with image and link
    - Introduced a new Button component with variants and styles.
    - Updated NotFound page to include a message, image, and a link back to home.
    - Added Maven Pro font to globals.css and applied it to the RootLayout.
    - Updated package.json and package-lock.json to include @radix-ui/react-slot dependency.
    - Added boat image to public directory.

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,5 +1,25 @@
-import React from "react";
+import Image from "next/image";
+import boatImg from "@/public/boat.png";
+import Link from "next/link";
+import { cn } from "@/lib/utils";
+import { buttonVariants } from "@/components/ui/button";
 
 export default function NotFound() {
-  return <div>not-found</div>;
+  return (
+    <main className="mx-auto w-full max-w-5xl py-20 text-center">
+      <h2 className="font-display text-4xl font-black">
+        Nada encontrado aqui...
+      </h2>
+      <Image className="mx-auto w-sm" src={boatImg} alt="Imagem de um barco" />
+      <p className="text-3xl font-extralight text-gray-400">
+        Não foi possível encontrar a página solicitada.
+      </p>
+      <Link
+        className={cn("mt-10 block", buttonVariants({ variant: "default" }))}
+        href="/"
+      >
+        Voltar à Home
+      </Link>
+    </main>
+  );
 }
