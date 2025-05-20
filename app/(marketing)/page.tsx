6e35196fa423a22642d8e8c434884be43c5cfe15
Caio Santana <brnoliveirasantana@gmail.com>
2025-05-20 20:33:08 -0300

    Add Marketing layout and enhance Home and Sobre pages with images and content

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,7 +1,24 @@
+import { Button } from "@/components/ui/button";
+import peopleImg from "@/public/people.png";
+import Image from "next/image";
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <div>
-      <h1>Home Page</h1>
-    </div>
+    <section className="text-center">
+      <h1 className="font-display mx-auto w-xl text-5xl font-black">
+        Diga adeus às longas buscas para uma vaga
+      </h1>
+      <Link href="/vagas">
+        <Button className="mt-12 cursor-pointer" variant="outline">
+          Busque uma Vaga
+        </Button>
+      </Link>
+      <Image
+        src={peopleImg}
+        alt="Ilustaração de pessoas trabalhando"
+        className="mx-auto mt-12"
+      />
+    </section>
   );
 }
